5208ffb233e69f299003cffcecf4032982383e68
CasShandu <cas.shandu@gmail.com>
2024-06-28 11:38:30 -0700

    0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,10 @@
+# This manifest creates a file 'school' in 'tmp'
 
-
-file {'/tmp/school':
+file { '/tmp/school':
   ensure  => present,
   content => 'I love Puppet',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
 }
+
