37d155578482891bd7aa8b537d987af626c8df94
CasShandu <cas.shandu@gmail.com>
2024-07-02 09:37:01 -0700

    100-puppet_ssh_config.pp

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# This manifest is configures the SSH client to use the SSH key for the user
+
+$config = "IdentityFile ~/.ssh/school
+PubKeyAuthentication yes
+PasswordAuthentication no
+"
+
+file {'/etc/ssh/ssh_config':
+  ensure  => present,
+  content => $config,
+}
